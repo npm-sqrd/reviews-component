c333823dcb9119ef71f7a1dac8869803eded4dac
Nicolas Peyrichou <nicolas.peyrichou@gmail.com>
2018-03-22 19:06:35 -0700

    node server

--- server/serverpurenode.js
+++ server/serverpurenode.js
@@ -2,30 +2,18 @@ const fs = require('fs');
 const http = require('http');
 const db = require('../db/mongodb');
 
-const html = fs.readFileSync('./react/dist/index.html', 'utf8');
-const css = fs.readFileSync('./react/dist/style.css', 'utf8');
-const bundle = fs.readFileSync('./react/dist/bundle-prod.js', 'utf8');
-
 const port = process.env.PORT || 3001;
 
-const files = {
-  'index.html': html,
-  'style.css': css,
-  'bundle-prod.js': bundle,
-};
-
-const filePathsAndType = {
-  '/': 'text/html',
-  '/index.html': 'text/html',
-  '/style.css': 'text/css',
-  '/bundle-prod.js': 'application/javascript',
-};
+const html = './react/dist/index.html';
+const css = './react/dist/style.css';
+const bundle = './react/dist/bundle-prod.js';
 
 http.createServer((req, res) => {
+  // GET request
   if (req.method === 'GET' && req.url.startsWith('/restaurants')) {
     const id = Number(req.url.slice(13));
     db.findByRestaurantId(id, (err, data) => {
-      if (err) {
+      if (err || id >= 10000000) {
         res.writeHead(404);
         res.write('Not Found!');
         res.end();
@@ -35,13 +23,45 @@ http.createServer((req, res) => {
         res.end(jsonString);
       }
     });
-  } else if (['/', '/index.html', '/style.css', '/bundle-prod.js'].includes(req.url)) {
-    res.writeHead(200, { 'Content-Type': filePathsAndType[req.url] });
-    res.end(req.url === '/' ? files['index.html'] : files[req.url.slice(1)]);
-  } else {
-    res.writeHead(404);
-    res.write('Not Found!');
-    res.end();
+  // index.html path
+  } else if (req.url === '/') {
+    fs.readFile(html, 'utf8', (err, file) => {
+      if (err) {
+        res.writeHead(404);
+        res.write(err.toString());
+        res.end();
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(file);
+        res.end();
+      }
+    });
+  // css.style path
+  } else if (req.url === '/style.css') {
+    fs.readFile(css, 'utf8', (err, file) => {
+      if (err) {
+        res.writeHead(404);
+        res.write(err.toString());
+        res.end();
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.write(file);
+        res.end();
+      }
+    });
+  // bundle.js path
+  } else if (req.url === '/bundle-prod.js') {
+    fs.readFile(bundle, 'utf8', (err, file) => {
+      if (err) {
+        res.writeHead(404);
+        res.write(err.toString());
+        res.end();
+      } else {
+        res.writeHead(200, { 'Content-Type': 'application/javascript' });
+        res.write(file);
+        res.end();
+      }
+    });
   }
 }).listen(port);
 console.log(`Node Server Up on port: ${port}`);
