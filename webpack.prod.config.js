80e15db3c81a9060fdee82c46e41fc46b5eef789
Nicolas Peyrichou <nicolas.peyrichou@gmail.com>
2018-03-23 16:43:57 -0700

    fixed newrelic

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,24 +1,40 @@
 const path = require('path');
-const Dotenv = require('dotenv-webpack');
 
-module.exports = {
-  entry: path.resolve(__dirname, './react/src/production.jsx'),
+const common = {
+  context: path.resolve(__dirname, './react/src'),
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
+
+const client = {
+  entry: path.resolve(__dirname, 'production.jsx'),
   output: {
     path: path.resolve(__dirname, './react/dist'),
     filename: 'bundle-prod.js',
   },
-  plugins: [
-    new Dotenv(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-    ],
+};
+
+const server = {
+  entry: path.resolve(__dirname, 'srcserver.js'),
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, './react/dist'),
+    filename: 'bundle-server.js',
+    libraryTarget: 'commonjs-module',
   },
 };
 
+module.exports = [
+  Object.assign({}, common, client),
+  Object.assign({}, common, server),
+];
