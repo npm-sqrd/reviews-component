faf4770d8be19562eed11a396f8add7c6fcd9fda
Nicolas Peyrichou <nicolas.peyrichou@gmail.com>
2018-03-27 13:35:45 -0700

    bug fixes and minor tweaks

--- react/src/reviewEntry.jsx
+++ react/src/reviewEntry.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 
-const ReviewEntry = (props) => {
-  return (
-    <div>
-      <div className="ratings">
-        <span>★</span><span>★</span><span>★</span><span>★</span><span>☆</span>|
+const ReviewEntry = props => (
+  <div>
+    <div className="ratings">
+      <span>★</span><span>★</span><span>★</span><span>★</span><span>☆</span>|
         Rating: {props.review.rating} | Username: {props.review.username} | City: {props.review.city} | Dined on {props.review.dinedDate.slice(0, 10)}
-      </div>
-      <div className="review">
-          {props.review.review}
-        <div className="wasThisUseful">Was this review useful to you?<span> Yes. No. Report</span></div>
+    </div>
+    <div className="review">{props.review.review}
+      <div className="wasThisUseful">
+        Was this review useful to you?<span> Yes. No. Report</span>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default ReviewEntry;
